86a649b1e0bbcc8697b4b0905d6a5b30c8b39c76
Jose Luis <pepirob>
2023-02-07 20:22:16 +0100

    styles added

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.static("public"))
+
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (req, res) => {
+  res.sendFile(__dirname + '/views/works.html')
 })
 
 app.listen(port, () => {
